c69f033ed773197790246f2fbaae7ff1a11843f9
Paul Louka <paul.louka@etu.toulouse-inp.fr>
2023-11-30 02:31:12 +0100

    Portable devices landscape support

--- script.js
+++ script.js
@@ -304,14 +304,13 @@ window.addEventListener('DOMContentLoaded', setMenuWidth);
 window.addEventListener('resize', updateLayout);
 if (window.screen && window.screen.orientation) {
     window.screen.orientation.addEventListener('change', function() {
-        setTimeout(updateLayout, 200); // wait for the orientation change to be effective
+        setTimeout(updateLayout, 300); // wait for the orientation change to be effective
     });
-} else {
-    // Fallback for browsers that don't support screen.orientation
-    window.addEventListener('resize', function() {
-        setTimeout(updateLayout, 200); // wait for the resize to be effective
-    });
-}
+};
+// Fallback for browsers that don't support screen.orientation
+window.addEventListener('resize', function() {
+    setTimeout(updateLayout, 300); // wait for the resize to be effective
+});
 
 
 // href corrections
